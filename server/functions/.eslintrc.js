bb59ed0f59fc1f363aadfb35fed3f49f9ab36f2b
sudkul <sudhanshu.kulshrestha@udacity.com>
2026-06-09 12:52:38 +0530

    Add server: Firebase Functions API for buildings/cities/countries
    Initial import of the server/ folder containing the Firebase Cloud
    Functions v2 Express API (db.json-backed) prior to any modification.
    
    Author: sudkul | Sudhanshu Kulshrestha

---
+++ server/functions/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "new-cap": ["error", { "properties": false, "newIsCap": false }],
+    "object-curly-spacing": ["error", "always", { "arraysInObjects": false }],
+    "max-len": ["error", { "code": 120 }],
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
